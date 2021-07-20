7e2028b8f2cabdecda2e94f270b048d3a1749680
czuhajster <18arek@gmail.com>
2021-07-20 13:16:43 +0200

    Update release version in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='garb',
-    version='0.2.1',
+    version='0.2.2',
     description='Request Builder for Google Analytics Reporting API v4 ',
     long_description=long_description,
     long_description_content_type='text/markdown',
