9e0dde500ef4c5c90990964c204ddb0d63883486
czuhajster <18arek@gmail.com>
2021-07-20 13:05:03 +0200

    Fix long description.

--- setup.py
+++ setup.py
@@ -7,8 +7,10 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='garb',
-    version='0.2.0',
+    version='0.2.1',
     description='Request Builder for Google Analytics Reporting API v4 ',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/czuhajster/garb',
     author='Arkadiusz Podkowa',
     author_email='18arek@gmail.com',
@@ -16,8 +18,8 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Google Analytics',
-        'Lisence :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.9'
     ],
     keywords='google analytics',
