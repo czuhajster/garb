d7c842075bf36b3c425611a72cc2473686442056
czuhajster <18arek@gmail.com>
2021-07-20 12:41:22 +0200

    Update setup.py and gitignore.

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
 setup(
     name='garb',
     version='0.2.0',
