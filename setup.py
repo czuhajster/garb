f0fa8f01338d5fb3a5482b46252d30b8b029cf48
czuhajster <18arek@gmail.com>
2021-07-20 12:30:21 +0200

    Add setup.py file.

---
+++ setup.py
@@ -0,0 +1,23 @@
+setup(
+    name='garb',
+    version='0.2.0',
+    description='Request Builder for Google Analytics Reporting API v4 ',
+    url='https://github.com/czuhajster/garb',
+    author='Arkadiusz Podkowa',
+    author_email='18arek@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Google Analytics',
+        'Lisence :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9'
+    ],
+    keywords='google analytics',
+    project_url={
+        'Tracker': 'https://github.com/czuhajster/garb/issues'
+    },
+    packages=find_packages(include=['garb', 'garb.*']),
+    python_requires='>=3.9'
+)
+
